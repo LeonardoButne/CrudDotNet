45f68b470ea4432254c57c34a8e0061b2fcabbd8
Leonardo <leonardobutne78@gmail.com>
2024-08-30 12:41:55 +0200

    2

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,5 +1,25 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace LeoTest.Models;
 
 public class Student{
 
+    [Key]
+    [DisplayName("ID")]
+
+    public int Id {get; set;}
+
+    [Required(ErrorMessage = "Informe o nome")]
+    [StringLength (80, ErrorMessage = "O nome deve conter ate 80 caracteres")]
+    [MinLength(5, ErrorMessage = "O nome deve conter pelo menos 5 caracteres")]
+    [DisplayName("Nome completo")]
+    public string Name {get; set;} = string.Empty;
+    
+    [Required(ErrorMessage = "Informe o E-mail")]
+    [EmailAddress(ErrorMessage = "E-mail invalido")]
+    [DisplayName("Email")]
+    public string Email {get; set;} = string.Empty;
+
+    public List <Premium> Premiums {get; set; } = new();
 } 
